0aee8144f45e7751567945fcb1a1c743203268c8
Thomas Leon HIghbaugh <thighbaugh@zoho.com>
2020-09-11 21:48:59 -0700

    updated

--- main/apps.lua
+++ main/apps.lua
@@ -36,7 +36,7 @@ local run_on_start_up = {
     'xfsettingsd & ',
     'sh dropbox-dist/dropboxd & ',
     -- the next one will be dependent on your set up, configure with arandr and then save, open that file and copy over the below
-    'xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 4480x0 --rotate normal --output DP-0 --off --output DP-1 --mode 1920x1080 --pos 2560x0 --rotate normal --output DP-2 --off --output DP-3 --off --output DP-4 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP-5 --off'
+    'xrandr --output DVI-D-1 --off --output DP-1 --primary --mode 2560x1440 --pos 3840x0 --rotate normal --output DP-2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-1 --off --output DP-3 --mode 1920x1080 --pos 0x0 --rotate normal'
 }
 
 -- ===================================================================
