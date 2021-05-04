7db5bbc2160f1244c6e71fd580ee3d3f493eea13
Thomas Leon Highbaugh <thighbaugh@zoho.com>
2021-05-03 21:10:19 -0700

    the closest its been ever to done

--- rc.lua
+++ rc.lua
@@ -1,4 +1,8 @@
 ---@diagnostic disable: lowercase-global
+
+-- ####################################################################
+-- Libraries & External Modules
+-- ####################################################################
 -- If LuaRocks is installed, make sure that packages installed through it are
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
@@ -10,40 +14,70 @@ local wibox = require("wibox")
 local beautiful = require("beautiful")
 local naughty = require("naughty")
 require("awful.hotkeys_popup.keys")
-require('awful.autofocus')
+require("awful.autofocus")
+-- Awesome Freedesktop
 local has_fdo, freedesktop = pcall(require, "freedesktop")
 
+-- ####################################################################
 -- Error handling
--- Check if awesome encountered an error during startup and fell back to
--- another config (This code will only ever execute for the fallback config)
+-- ####################################################################
+-- Check if awesome encountered an error during startup
 require("module.error-handling")
-beautiful.init( gears.filesystem.get_configuration_dir() .. "/themes/vice/theme.lua")
 
+-- ####################################################################
+-- Initialize Theme
+-- ####################################################################
+beautiful.init(gears.filesystem.get_configuration_dir() .. "/themes/vice/theme.lua")
+
+-- ####################################################################
+-- Set Global Defaults
+-- ####################################################################
 terminal = "kitty"
 editor = os.getenv("EDITOR") or "nvim"
 editor_cmd = terminal .. " -e " .. editor
 modkey = "Mod4"
 
--- Table of layouts to cover with awful.layout.inc, order matters.
+-- ####################################################################
+-- Layouts
+-- ####################################################################
 require("configurations.layouts")
 
+-- ####################################################################
 -- Wibar
+-- ####################################################################
 require("configurations.wibar.wibox")
+
+-- ####################################################################
 -- Mouse bindings
+-- ####################################################################
 root.buttons = require("configurations.keybindings.buttons.global")
 clientbuttons = require("configurations.keybindings.buttons.client")
 
+-- ####################################################################
 -- Key bindings
+-- ####################################################################
 globalkeys = require("configurations.keybindings.global")
 clientkeys = require("configurations.keybindings.client")
-
 -- Set keys
 root.keys(globalkeys)
 
+-- ####################################################################
 -- Rules
+-- ####################################################################
 require("configurations.rules")
 require("module.signals")
-require("widgets.exit-screen")
-require("module.notification")
-require("module.autostart")
 
+-- ####################################################################
+-- Exit Screen
+-- ####################################################################
+require("widgets.exit-screen")
+
+-- ####################################################################
+-- Notifications
+-- ####################################################################
+require("module.notification")
+
+-- ####################################################################
+-- Autostarting Applications with WM
+-- ####################################################################
+require("module.autostart")
--- themes/vice/theme.lua
+++ themes/vice/theme.lua
@@ -88,6 +88,7 @@ theme.btn_xs_margin = dpi(3)
 theme.btn_md_margin = dpi(5)
 theme.widget_margin = dpi(3)
 theme.is_enable_titlebar = true
+
 -- ####################################################################
 -- shapes
 -- ####################################################################
@@ -105,9 +106,18 @@ end
 theme.panel_button_shape = function(cr, height, width)
     gears.shape.rounded_rect(cr, height, width, 1)
 end
-
+-- ####################################################################
+-- MStab Configuration
+-- ####################################################################
 theme.mstab_tabbar_style = "modern"
+theme.mstab_bar_ontop = true -- whether you want to allow the bar to be ontop of clients
+theme.mstab_bar_padding = dpi(0) -- how much padding there should be between clients and your tabbar
+theme.mstab_dont_resize_slaves = false
+theme.mstab_bar_height = 24
+
+-- ####################################################################
 -- Icons
+-- ####################################################################
 theme.icon_times = themes_path .. "icons/remove.svg"
 -- awesome icon
 theme.awesome_menu_icon = themes_path .. "icons/logo.svg"
@@ -119,42 +129,42 @@ theme.icon_previous = themes_path .. "icons/go-previous.svg"
 theme.icon_next = themes_path .. "icons/go-next.svg"
 
 theme.icon_focus_mode = themes_path .. "icons/focus-mode.svg"
-
 theme.brightness_icon = themes_path .. "icons/brightness.svg"
-
+-- battery
 theme.battery_half_icon = themes_path .. "icons/battery/battery-half.svg"
 theme.battery_empty_icon = themes_path .. "icons/battery/battery-empty.svg"
 theme.battery_icon = themes_path .. "icons/battery/battery-normal.svg"
+-- volume
 theme.volume_normal_icon = themes_path .. "icons/volume/audio-volume-high.svg"
 theme.volume_muted_icon = themes_path .. "icons/volume/audio-volume-muted.svg"
-
+-- wireless
 theme.wireless_connected_icon = themes_path .. "icons/network-wireless-connected.svg"
 theme.wireless_disconnected_icon = themes_path .. "icons/network-wireless-disconnected.svg"
 theme.wired_connected_icon = themes_path .. "icons/network-wired-activated.svg"
 theme.wired_disconnected_icon = themes_path .. "icons/network-wired-unavailable.svg"
-
+-- arrpws
 theme.arrow_left_icon = themes_path .. "icons/go-previous.svg"
 theme.arrow_right_icon = themes_path .. "icons/go-next.svg"
 theme.icon_terminal = themes_path .. "icons/utilities-terminal.svg"
-
+-- bluetooth
 theme.icon_bluetooth = themes_path .. "icons/network-bluetooth.svg"
 theme.icon_empty_notibox = themes_path .. "icons/mail-receive.svg"
 theme.icon_clear_all = themes_path .. "icons/clear-all.svg"
 theme.icon_camera = themes_path .. "icons/camera-photo.svg"
 theme.icon_mic = themes_path .. "icons/audio-input-microphone.svg"
 theme.icon_airplane = themes_path .. "icons/airplane-mode.svg"
-
+-- power
 theme.icon_system_power_off = themes_path .. "icons/system/system-shutdown.svg"
 theme.icon_system_restart = themes_path .. "icons/system/system-restart.svg"
 theme.icon_system_sleep = themes_path .. "icons/system/system-suspend.svg"
 theme.icon_system_screen_lock = themes_path .. "icons/system/system-lock-screen.svg"
 theme.icon_system_logout = themes_path .. "icons/system/system-log-out.svg"
 theme.face_image = themes_path .. "icons/system/default.png"
-
+-- notification center
 theme.icon_bell = themes_path .. "icons/notifications.svg"
 theme.icon_lock = themes_path .. "icons/lock.svg"
-theme.icon_redshift = themes_path .. "icons/redshift-status-on.svg"
 
+-- Floating
 theme.icon_floating = themes_path .. "layouts/floating.svg"
 
 theme.icon_cpu = themes_path .. "icons/cpu.svg"
@@ -165,24 +175,24 @@ theme.icon_screenhost_taken = themes_path .. "icons/64x64/camera-photo.svg"
 theme.icon_noti_error = themes_path .. "icons/48x48/dialog-error.svg"
 theme.icon_noti_info = themes_path .. "icons/48x48/dialog-information.svg"
 theme.icon_bat_caution = themes_path .. "icons/48x48/battery-caution.svg"
+-- ####################################################################
 -- Taglist
+-- ####################################################################
 --- Generate taglist squares:
 local taglist_square_size = dpi(10)
 theme.taglist_squares_sel = theme_assets.taglist_squares_sel(taglist_square_size, theme.xforeground)
 theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(taglist_square_size, theme.fg_normal)
 theme.taglist_bg_focus = theme.border_button
-theme.taglist_spacing = 4
+theme.taglist_spacing = 2
+--colors
 theme.taglist_fg_focus = theme.xcolor15
-
 theme.taglist_fg_urgent = theme.xcolor1
-
 theme.taglist_fg_occupied = theme.xforeground
-
 theme.taglist_fg_empty = theme.xcolor8
-
 theme.taglist_fg_volatile = theme.xcolor11
-
+-- ####################################################################
 -- Tasklist
+-- ####################################################################
 theme.tasklist_shape = function(cr, height, width)
     gears.shape.rounded_rect(cr, height, width, 6)
 end
@@ -191,7 +201,9 @@ theme.tasklist_shape_border_color = theme.border_button
 theme.tasklist_bg_focus = theme.bg_normal
 theme.tasklist_bg_normal = theme.bg_button
 
+-- ####################################################################
 -- Snap area
+-- ####################################################################
 theme.snap_bg = theme.bg_button
 theme.snap_shape = function(cr, height, width)
     gears.shape.rounded_rect(cr, height, width, 8)
@@ -199,7 +211,9 @@ end
 theme.snap_border_width = dpi(3)
 theme.snapper_gap = dpi(3)
 
+-- ####################################################################
 -- Notification
+-- ####################################################################
 theme.notification_font = theme.font
 theme.notification_bg = theme.bg_normal
 theme.notification_fg = theme.fg_normal
@@ -211,42 +225,51 @@ theme.notification_max_width = dpi(465)
 theme.notification_icon_size = 64
 theme.notification_spacing = dpi(8)
 theme.notification_border_width = 0
---theme.notification_border_color  = theme.border_normal
+theme.notification_border_color = theme.fg_focus
 
+-- ####################################################################
 -- systray
+-- ####################################################################
 theme.bg_systray = theme.bg_normal
 
+-- ####################################################################
 -- Menu
+-- ####################################################################
 theme.menu_submenu_icon = themes_path .. "/submenu.png"
 theme.menu_height = dpi(240)
 theme.menu_width = dpi(200)
 
+-- ####################################################################
 -- Wibar
+-- ####################################################################
 theme.wibar_height = dpi(28)
 theme.wibar_position = "bottom"
 
+-- ####################################################################
 -- Titlebar
+-- ####################################################################
 --- Define the image to load
 theme.titlebar_close_button_normal = themes_path .. "/titlebar/window-close-normal.svg"
 theme.titlebar_close_button_focus = themes_path .. "/titlebar/window-close.svg"
-
 theme.titlebar_minimize_button_normal = themes_path .. "/titlebar/go-down.svg"
 theme.titlebar_minimize_button_focus = themes_path .. "/titlebar/go-down.svg"
-
 theme.titlebar_sticky_button_normal_inactive = themes_path .. "/titlebar/window-pin.svg"
 theme.titlebar_sticky_button_focus_inactive = themes_path .. "/titlebar/window-pin.svg"
 theme.titlebar_sticky_button_normal_active = themes_path .. "/titlebar/window-pin.svg"
 theme.titlebar_sticky_button_focus_active = themes_path .. "/titlebar/window-pin.svg"
-
 theme.titlebar_maximized_button_normal_inactive = themes_path .. "/titlebar/go-up.svg"
 theme.titlebar_maximized_button_focus_inactive = themes_path .. "/titlebar/go-up.svg"
 theme.titlebar_maximized_button_normal_active = themes_path .. "/titlebar/go-up.svg"
 theme.titlebar_maximized_button_focus_active = themes_path .. "/titlebar/go-up.svg"
 theme.titlebar_bg_focus = theme.bg_normal
-
+-- ####################################################################
+-- wallpaper
+-- ####################################################################
 theme.wallpaper = themes_path .. "/images/5.png"
 
+-- ####################################################################
 -- LayoutBox
+-- ####################################################################
 --- You can use your own layout icons like this:
 theme.layout_floating = themes_path .. "layouts/floating.svg"
 theme.layout_max = themes_path .. "layouts/max.svg"
@@ -254,7 +277,9 @@ theme.layout_fullscreen = themes_path .. "layouts/fullscreen.svg"
 theme.layout_tile = themes_path .. "layouts/tile.svg"
 theme.layout_dwindle = themes_path .. "layouts/dwindle.svg"
 
+-- ####################################################################
 -- Hotkey popup
+-- ####################################################################
 theme.hotkeys_border_width = dpi(1)
 theme.hotkeys_border_color = theme.bg_normal
 theme.hotkeys_modifiers_fg = theme.xforeground
